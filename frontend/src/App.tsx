e1544e82a6715ece6a5cae19e0571eea0542e727
LEE CHI <86901868+LeeRichi@users.noreply.github.com>
2025-10-05 23:01:59 +0300

    set up folder structure (#2)
    - configed tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <h1>Yoooo let's start our frontend here!</h1>
+      <h3 className="text-3xl font-bold text-blue-600">Tailwind is working!</h3>
+    </>
+  )
+}
+
+export default App
