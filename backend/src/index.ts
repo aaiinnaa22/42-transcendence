ef88cc40f1520fba3bed1af053c7462292192e8b
Fred Pikkov <fpikkov@gmail.com>
2025-10-09 17:21:06 +0300

    Added Prisma as a Fastify plugin.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import Fastify, { type FastifyInstance, type RouteShorthandOptions } from 'fastify';
+import prismaPlugin from './plugins/prisma.ts';
 
 const server : FastifyInstance = Fastify({
 	logger: true
 });
 
+// ===================== TESTING =========================
 const opts : RouteShorthandOptions = {
 	schema: {
 		response: {
@@ -30,14 +32,32 @@ server.get('/', opts, async (request, response) => {
 		response.code(403).send({ message: 'forbidden access'});
 	}
 });
+// ========================================================
 
+
+// Main of the backend server
 const start = async () => {
 	try {
+		await server.register(prismaPlugin);
 		await server.listen({ port: 6000, host: '127.0.0.1' });
 	} catch (error) {
 		server.log.error(error);
 		process.exit(1);
 	}
 }
+
+// Handler to shut down prisma properly on being signaled
+const stop = async () => {
+	try {
+		await server.close();
+	} catch (error) {
+		server.log.error(error);
+	} finally {
+		process.exit(0);
+	}
+}
+
+process.on('SIGINT', stop);
+process.on('SIGTERM', stop);
 
 start();
