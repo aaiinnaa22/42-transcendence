5c68e5d205c8a8675178c32028868e2e2faa3ecb
Aina Albrecht <aalbrech@c2r2p13.hive.fi>
2025-10-15 14:28:11 +0300

    ping pong animation added + cleanup

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -9,25 +9,18 @@ type NavBarProps =
 	onNavigate: (page: Page) => void;
 };
 
-/*
-TRY NAVBAR HEIGHT IN PERCENTAGE OF SCREEN INSTEAD?
-
-underline effect: (placed under the h2)
-<div className={"mt-2 h-1.25 w-10 rounded-full transition-all duration-100 " +
-	(isActive ? "bg-black" : "bg-transparent group-hover:bg-gray-600")}>
-</div>
-*/
-
 export const NavBar = ({currentPage, onNavigate}: NavBarProps) =>
 {
 	return (
 		<div className="flex flex-row items-center justify-between h-18 lg:h-32 bg-transcendence-beige px-10">
 			<h1 className="font-transcendence-one font-extrabold text-5xl text-transcendence-black tracking-[0.8rem]">PONG</h1>
-			<div className="hidden h-full lg:flex flex-row sm:gap-5 md:gap-10 lg:gap-20 font-transcendence-two text-2xl">
+			<div className="hidden h-full lg:flex flex-row sm:gap-5 md:gap-10 lg:gap-20 2xl:gap-30 font-transcendence-two text-2xl">
 				{navItems.map((item) => {
 					const isActive = currentPage == item;
 					return (
-						<div onClick={() => onNavigate(item)}
+						<div
+							key={item} 
+							onClick={() => onNavigate(item)}
 							className={"mt-16 rounded-tl-xl rounded-tr-xl px-2 py-2 cursor-pointer border-2 "
 							+ (isActive ? "bg-white border-b-0 border-black" : "hover:pt-3 border-transcendence-beige")}>
 							<h2 className="text-black font-normal text-center">
--- frontend/src/components/PlayButton.tsx
+++ frontend/src/components/PlayButton.tsx
@@ -8,12 +8,11 @@ type PlayButtonProps =
 export const PlayButton = ({startGame}: PlayButtonProps) =>
 (
 	<div className="flex flex-grow flex-row items-center justify-center">
-		{/* group relative*/}
 		<button 
 			className="group relative bg-transcendence-beige rounded-[3rem] w-70 h-40 text-transcendence-black font-transcendence-three tracking-[0.1em] text-5xl cursor-pointer overflow-hidden"
 			onClick={startGame}>
 			PLAY
-			{/*<span className="absolute top-[75%] left-[15%] w-4 h-4 border-2 border-transcendence-black bg-white rounded-full transform -translate-y-1/2 group-hover:animate-[pingpong_0.6s_ease-in-out_forwards]" />*/}
+			<span className="absolute bottom-[35%] right-[26%] w-4 h-4 border-2 border-transcendence-black bg-white rounded-full transform -translate-y-1/2 group-hover:animate-[pingpongAnimation_1s_ease-in-out]" />
 		</button>
 	</div>
 );
