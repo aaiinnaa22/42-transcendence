663044c1c2efa410a6dad60b91cb2e376068b1fb
Aina Albrecht <162609425+aaiinnaa22@users.noreply.github.com>
2025-10-15 15:58:02 +0300

    Frontend website (#5)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,44 @@ import './App.css'
 import './index.css'
 import {NavBar} from "./components/NavBar";
 import {PlayButton} from "./components/PlayButton";
+import {Leaderboard} from "./components/Leaderboard";
+import {Tournament} from "./components/Tournament";
 
 function App() {
-  return (
-   <div className="bg-transcendence-black h-screen w-screen flex flex-col gap-70">
-		<NavBar></NavBar>
-		<PlayButton></PlayButton>
-	</div>
+	const [isGameOn, setGameOn] = useState(false);
+	type Page = "play" | "leaderboard" | "tournament";
+	const [currentPage, setPage] = useState<Page>("play");
+
+	const renderPage = () => {
+		switch (currentPage)
+		{
+			case "leaderboard":
+				return <Leaderboard></Leaderboard>;
+			case "tournament":
+				return <Tournament></Tournament>;
+			default:
+				return <PlayButton startGame={() => setGameOn(true)} ></PlayButton>;
+		}
+	};
+	return (
+		<div className="bg-transcendence-black min-h-screen w-full flex flex-col">
+			<NavBar currentPage={currentPage} onNavigate={setPage}></NavBar>
+			{renderPage()};
+		</div>
   )
 }
+
+/*function App() {
+	const [isGameOn, setGameOn] = useState(false);
+	return (
+	<div className="bg-transcendence-black min-h-screen w-full flex flex-col">
+		<NavBar></NavBar>
+		{!isGameOn ?
+			(<PlayButton startGame={() => setGameOn(true)}></PlayButton>) :
+			(<GameComponent exitGame={() => setGameOn(false)}></GameComponent>)
+		}
+	</div>
+  )
+}*/
 
 export default App
--- frontend/src/components/PlayButton.tsx
+++ frontend/src/components/PlayButton.tsx
@@ -1,7 +1,18 @@
 
-export const PlayButton = () =>
+type PlayButtonProps =
+{
+	startGame: () => void;
+};
+
+
+export const PlayButton = ({startGame}: PlayButtonProps) =>
 (
-	<div className="flex flex-row items-center justify-center">
-		<button className= "bg-transcendence-beige rounded-[3rem] w-70 h-40 text-transcendence-black font-transcendence-three tracking-[0.1em] text-5xl">PLAY</button>
+	<div className="flex flex-grow flex-row items-center justify-center">
+		<button 
+			className="group relative bg-transcendence-beige rounded-[3rem] w-70 h-40 text-transcendence-black font-transcendence-three tracking-[0.1em] text-5xl cursor-pointer overflow-hidden"
+			onClick={startGame}>
+			PLAY
+			<span className="absolute bottom-[35%] right-[26%] w-4 h-4 border-2 border-transcendence-black bg-white rounded-full transform -translate-y-1/2 group-hover:animate-[pingpongAnimation_1s_ease-in-out]" />
+		</button>
 	</div>
 );
