64370883da23f148029dea8359232fa77eddcb62
Aina Albrecht <aalbrech@c2r2p13.hive.fi>
2025-10-16 13:33:09 +0300

    New place for game in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,13 +5,15 @@ import {NavBar} from "./components/navbar/NavBar";
 import {PlayButton} from "./components/PlayButton";
 import {Leaderboard} from "./components/Leaderboard";
 import {Tournament} from "./components/Tournament";
+import {Game} from "./components//Game";
 
 function App() {
 	const [isGameOn, setGameOn] = useState(false);
 	type Page = "play" | "leaderboard" | "tournament";
 	const [currentPage, setPage] = useState<Page>("play");
 
-	const renderPage = () => {
+	const renderPage = () =>
+	{
 		switch (currentPage)
 		{
 			case "leaderboard":
@@ -19,28 +21,17 @@ function App() {
 			case "tournament":
 				return <Tournament></Tournament>;
 			default:
-				return <PlayButton startGame={() => setGameOn(true)} ></PlayButton>;
+				return !isGameOn
+					? (<PlayButton startGame={() => setGameOn(true)} ></PlayButton>)
+					: (<Game exitGame={() => setGameOn(false)}></Game>);
 		}
-	};
+	}
 	return (
 		<div className="bg-transcendence-black min-h-screen w-full flex flex-col">
 			<NavBar currentPage={currentPage} onNavigate={setPage}></NavBar>
 			{renderPage()};
 		</div>
-  )
-}
-
-/*function App() {
-	const [isGameOn, setGameOn] = useState(false);
-	return (
-	<div className="bg-transcendence-black min-h-screen w-full flex flex-col">
-		<NavBar></NavBar>
-		{!isGameOn ?
-			(<PlayButton startGame={() => setGameOn(true)}></PlayButton>) :
-			(<GameComponent exitGame={() => setGameOn(false)}></GameComponent>)
-		}
-	</div>
-  )
-}*/
+	)
+};
 
 export default App
