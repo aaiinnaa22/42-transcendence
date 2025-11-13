85d57ae18f88e03e3255a7bd4a412dbe57f105da
Leo Laaksonen <leo.laaksonen@hotmail.com>
2025-11-13 17:34:24 +0200

    Pong backend (#24)
    * Setting up fastify websockets and testing
    
    * Game and Player class added, backend sever can now launch multiple individual games
    
    * Ball class with collision checks implemented and front end now uses the back end pong server
    
    * Readability of the code updated and front end now only makes one connection to the back end
    
    * Implement pong backend server with Fastify websockets and frontend integration
    
    * Revert "Included pong container build target in the makefile"
    
    * Changed to pong directory to backend/src
    
    ---------
    
    Co-authored-by: Fred Pikkov <fpikkov@gmail.com>

---
+++ backend/src/pong/game.js
@@ -0,0 +1,95 @@
+'use strict';
+
+const Player = require('./player.js');
+const Ball = require('./ball.js');
+const { WIDTH, HEIGHT, BALL_SIZE, PADDLE_LEN, PADDLE_WIDTH } = require('./constants');
+
+class Game {
+    constructor(id) 
+    {
+        this.id = id;
+        this.players = [];
+        this.ball = new Ball;
+    }
+
+    addPlayer() 
+    {
+        const newPlayer = new Player(1, 10, HEIGHT/2);
+        this.players.push(newPlayer);
+        const newPlayer2 = new Player(2, 1580, HEIGHT/2);
+        this.players.push(newPlayer2);
+        //console.log("Players added");
+    }
+
+    movePlayer(playerId, dx, dy) 
+    {
+        const player = this.players.find(p => p.id === playerId);
+        if (player) 
+        {
+            player.move(dx, dy);  // Move the player by dx, dy
+        }
+    }
+
+    moveBall()
+    {
+        this.ball.x += this.ball.vx;
+        this.ball.y += this.ball.vy;
+
+        //check if ball is colliding Player 1
+        if (this.ball.x >= this.players[0].x && this.ball.x <= this.players[0].x + PADDLE_WIDTH && this.ball.y >= this.players[0].y && this.ball.y <= this.players[0].y + PADDLE_LEN)
+        {
+            const playerCenter = this.players[0].y + PADDLE_LEN/2;
+            this.ball.vy = (playerCenter - this.ball.y) * 0.1;
+            this.ball.vx *= -1;
+        }
+        //check if ball is colliding Player 2
+        if (this.ball.x + BALL_SIZE >= this.players[1].x && this.ball.x <= this.players[1].x + PADDLE_WIDTH && this.ball.y + BALL_SIZE >= this.players[1].y && this.ball.y <= this.players[1].y + PADDLE_LEN)
+        {
+            const playerCenter2 = this.players[1].y + PADDLE_LEN/2;
+            this.ball.vy = (playerCenter2 - this.ball.y) * 0.1;
+            this.ball.vx *= -1;
+        }
+        //Check if ball is inside of a goal and resets ball position if there is a goal
+        if (this.ball.x <= 0 || this.ball.x >= WIDTH)
+        {
+            if (this.ball.x <= 0)
+            {
+                this.players[0].points += 1;
+            }
+            else
+            {
+               this.players[1].points += 1;
+            }
+            //check if we have winner here
+            //Need to figure out what to do when the game is ended
+            if (this.players[0].points >= 5 || this.players[1].points >= 5)
+            {
+                this.players[0].points = 42; // Figure out correct way to end the game currently just a place holder
+            }
+            this.ball.resetBall();
+            return ;
+        }
+        //Check collision with walls
+        if (this.ball.y <= 0 || this.ball.y >= HEIGHT)
+        {
+            this.ball.vy *= -1;
+        }
+    }
+
+    getState() 
+    {
+    const playersState = {};
+
+    this.players.forEach(player => {
+        playersState[player.id] = player.getState(); 
+    });
+
+    return {
+        type: 'state',
+        players: playersState,
+        ball: this.ball.getState(),
+    };
+}
+}
+
+module.exports = Game;
