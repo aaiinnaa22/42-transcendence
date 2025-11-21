7dbdbcb5cd050d23101355b02fb852a4558e556d
Hanna Skrzypiec <h.skrzypiec@gmail.com>
2025-11-21 17:11:35 +0200

    db quick fix

--- backend/entrypoint.dev.sh
+++ backend/entrypoint.dev.sh
@@ -13,15 +13,31 @@ npx prisma generate
 # Apply migrations
 npx prisma migrate dev || echo "Migrations skipped..."
 
-# Seeding the development database
 SEED_MARKER="/app/prisma/.seeded"
 
+echo "Checking if database is seeded..."
+DB_ALREADY_SEEDED=false
+
+if npx prisma db execute --stdin >/dev/null 2>&1 <<EOF
+SELECT 1 FROM "User" LIMIT 1;
+EOF
+then
+  DB_ALREADY_SEEDED=true
+fi
+
+# Seeding if 1. seed file exists 2. marker file doesn't exist 3. db is empty
 if { [ -f /app/prisma/seed.ts ] || [ -f /app/prisma/seed.js ]; } \
-  && [ ! -f "$SEED_MARKER" ]; then
+  && [ ! -f "$SEED_MARKER" ] \
+  && [ "$DB_ALREADY_SEEDED" = "false" ]; then
     echo "Seeding database..."
-    npx prisma db seed && touch "$SEED_MARKER" || echo "Seeding failed or skipped"
-  else
-    echo "Database already seeded..."
+    if npx prisma db seed; then
+      echo "Seed successful — marking as seeded."
+      touch "$SEED_MARKER"
+    else
+      echo "Seeding failed or skipped."
+    fi
+else
+  echo "Database already seeded — skipping."
 fi
 
 echo "Starting backend..."
