ae9d6afc5dadd2e8e4dc68940802202cef1d4332
Leo Laaksonen <leo.laaksonen@hotmail.com>
2025-11-24 14:24:48 +0200

    single and multiplayer game working

--- frontend/src/components/home/game/Game.tsx
+++ frontend/src/components/home/game/Game.tsx
@@ -88,11 +88,8 @@ export const Game = () =>
 
     useEffect(() => {
         let animationFrameId: number; // not needed ??
-<<<<<<< HEAD
         const ws = new WebSocket('ws://localhost:4241/game/singleplayer');
-=======
-        const ws = new WebSocket('ws://localhost:4241/game/multiplayer');
->>>>>>> main
+
         wsRef.current = ws;
 
         const handleKeyDown = (e: KeyboardEvent) => { keysPressed.current[e.key] = true; };
