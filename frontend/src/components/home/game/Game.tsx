e204323db6e58aff931756012cb841eff8db7852
Leo Laaksonen <leo.laaksonen@hotmail.com>
2025-11-27 12:11:05 +0200

    Game room inactivity timeout (#51)
    * Game room inactivity timeout
    
    * Restored previous version of ball.ts
    
    ---------
    
    Co-authored-by: Fred Pikkov <fpikkov@gmail.com>

--- frontend/src/components/home/game/Game.tsx
+++ frontend/src/components/home/game/Game.tsx
@@ -116,6 +116,11 @@ export const Game = () =>
 			{
 				console.log("You are already in a match.");
 			}
+			else if (data.type === "inactivity")
+			{
+				console.log("Game ended due to inactivity.");
+				console.log("Winner is player " + data.winner);
+			}
 			/* ADD ADDITIONAL STATES HERE */
 		};
 
