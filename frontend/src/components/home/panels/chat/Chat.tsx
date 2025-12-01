813d41a5e53506a9d51c801ce7c460492b0e5899
Aina Albrecht <162609425+aaiinnaa22@users.noreply.github.com>
2025-12-01 14:52:10 +0200

    Chat added (#55)
    First version of chat with example users

---
+++ frontend/src/components/home/panels/chat/Chat.tsx
@@ -0,0 +1,51 @@
+import { ChatProfilePic } from "./ChatProfilePic";
+
+type Friend = {
+	username: string;
+	profile: string;
+	online: boolean;
+	lastMessage?: string;
+};
+
+const Friends: Friend[] = [
+	{
+		username: "Aina",
+		profile: "/profiles/aina",
+		online: true,
+		lastMessage: "Let's play!"
+	},
+	{
+		username: "Anna",
+		profile: "",
+		online: false,
+		lastMessage: "Let's play later!"
+	}
+];
+
+type ChatProps = {
+	onChatClick: () => void;
+};
+
+
+export const Chat = ({onChatClick}: ChatProps) => {
+	//TODO: I need to get a list from backend of all the players friends and their username, profile, online, lastmessage (should include WHO sent the last message)...
+	return (
+		<div className="flex flex-col px-4">
+			<ul className="flex flex-col">
+				{Friends.map((friend) => (
+					<li key={friend.username} className="border-b-2">
+						<button onClick={onChatClick}
+							className="flex flex-row gap-3 py-2 text-left cursor-pointer">
+							<ChatProfilePic friend={friend}/>
+							<div className="flex flex-col gap-1">
+								<h3 className="font-semibold text-md">{friend.username}</h3>
+								<p className="text-xs">Julia: {friend.lastMessage}</p>
+							</div>
+						</button>
+					</li>
+				))}
+
+			</ul>
+		</div>
+	);
+}
