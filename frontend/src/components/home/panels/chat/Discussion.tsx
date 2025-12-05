e2e4c2ec6e6a25ff564e5c5cd729347133702314
Aina A <aina.elona.albrecht@gmail.com>
2025-12-05 18:12:57 +0200

    added responsiveness for chatting

--- frontend/src/components/home/panels/chat/Discussion.tsx
+++ frontend/src/components/home/panels/chat/Discussion.tsx
@@ -63,14 +63,14 @@ export const Discussion = ({onExitClick}: DiscussionProps) =>
 
 			{/* Header */}
 			<div className="flex flex-row justify-between items-center bg-white w-full lg:rounded-tl-xl  p-2 border-b-2">
-				<button onClick={onExitClick} className="material-symbols-outlined !text-md">arrow_back_ios_new</button>
-				<h2 className="font-semibold">{friend.username}</h2>
+				<button onClick={onExitClick} className="material-symbols-outlined !text-xs lg:!text-md">arrow_back_ios_new</button>
+				<h2 className="font-semibold text-sm lg:text-md">{friend.username}</h2>
 				<ChatProfilePic friend={friend}/>
 			</div>
-			<div className="self-end p-3">
+			<div className="self-end p-2">
 				<button className="px-3 flex flex-row items-center justify-between rounded-4xl gap-2 bg-transcendence-white border-2 cursor-pointer">
-				<p className="text-sm">Invite {friend.username} to a game</p>
-				<div className="!text-3xl material-symbols-outlined">
+				<p className="text-xs lg:text-sm">Invite {friend.username} to a game</p>
+				<div className="!text-xl lg:!text-3xl material-symbols-outlined">
 				sports_esports</div>
 				</button>
 			</div>
@@ -81,7 +81,7 @@ export const Discussion = ({onExitClick}: DiscussionProps) =>
 					<div
 						key={msg.id}
 						className={"p-3 rounded-lg  max-w-[70%] bg-white " + (msg.sender === "me" ? "self-end" : "self-start")}>
-						<p className="break-words">{msg.text}</p>
+						<p className="break-words text-xs lg:text-md">{msg.text}</p>
 					</div>
 				))}
 				<div ref={discussionEndRef}></div>
@@ -90,7 +90,7 @@ export const Discussion = ({onExitClick}: DiscussionProps) =>
 			{/* Input bar */}
 			<div className="p-4">
 				<form onSubmit={handleMessageSubmit} className="flex gap-2 w-full justify-center">
-				<div className="bg-white rounded-lg border min-h-8 max-h-40 overflow-y-auto w-[70%] xl:w-full">
+				<div className="bg-white rounded-lg border min-h-8 max-h-40 overflow-y-auto w-[70%] xl:w-full text-xs lg:text-md">
 					<textarea
 					ref={textAreaRef}
 					value={message}
