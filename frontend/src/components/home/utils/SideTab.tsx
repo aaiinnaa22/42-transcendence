fcfafa2215e4d3e44c1d7ee3c3d43f4ed573eead
Aina A <aina.elona.albrecht@gmail.com>
2025-12-11 12:20:03 +0200

    leaderboard overlapping chat fixed

--- frontend/src/components/home/utils/SideTab.tsx
+++ frontend/src/components/home/utils/SideTab.tsx
@@ -8,7 +8,7 @@ export const SideTab = ({isOpen, children}: SideTabProps) =>
 {
 	return (
 		<div
-			className={"fixed hidden xl:block top-33 right-0 h-[80%] w-70 bg-transcendence-beige rounded-tl-2xl rounded-bl-2xl transform transition-transform duration-400 "
+			className={"z-40 fixed hidden lg:block top-33 right-0 h-[80%] w-70 bg-transcendence-beige rounded-tl-2xl rounded-bl-2xl transform transition-transform duration-400 "
 			+ (isOpen ? "translate-x-0" : "translate-x-[100%]")}>
 			{children}
 		</div>
