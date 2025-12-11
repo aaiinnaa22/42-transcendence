e1e7b0dcb9e5e317667adfe6c348622213a0f853
Aina A <aina.elona.albrecht@gmail.com>
2025-12-11 12:05:11 +0200

    choose game mode buttons do not change size on press anymore

--- frontend/src/components/home/game/ChooseGameMode.tsx
+++ frontend/src/components/home/game/ChooseGameMode.tsx
@@ -14,14 +14,14 @@ export const ChooseGameMode = ({onSinglePlayerChoose, onTournamentChoose}: Choos
 			<div className="flex flex-row col-start-2 row-start-2 gap-10 lg:gap-40 landscape:pt-[15vh] lg:landscape:pt-[20vh] justify-center
 			portrait:flex-col portrait:items-center portrait:gap-20 portrait:pt-0">
 				<div className="flex flex-col font-transcendence-two gap-6 text-center max-w-70">
-					<button className="rounded-2xl flex flex-row justify-center items-center gap-3 p-6 xl:p-8 cursor-pointer border-3 border-transcendence-white group landscape:h-10 lg:landscape:h-25 xl:h-25" onClick={onSinglePlayerChoose}>
+					<button className="rounded-2xl flex flex-row justify-center items-center gap-3 p-6 xl:p-8 cursor-pointer border-3 border-transcendence-white group h-22 landscape:h-15 md:landscape:h-25 xl:h-25" onClick={onSinglePlayerChoose}>
 						<span className="material-symbols-outlined text-transcendence-white w-5">arrow_forward_ios</span>
 						<h2 className="text-2xl text-transcendence-white group-hover:pt-1">play together</h2>
 					</button>
 					<p className="italic text-transcendence-white">play together with your friend on the same screen</p>
 				</div>
 				<div className="flex flex-col font-transcendence-two gap-6 text-center">
-					<button className="rounded-2xl flex flex-row justify-center items-center gap-3 p-6 xl:p-8 border-3 border-transcendence-white cursor-pointer group landscape:h-10 lg:landscape:h-25 xl:h-25"
+					<button className="rounded-2xl flex flex-row justify-center items-center gap-3 p-6 xl:p-8 border-3 border-transcendence-white cursor-pointer group h-22 landscape:h-15 md:landscape:h-25 xl:h-25"
 						onClick={onTournamentChoose}>
 						<span className="material-symbols-outlined text-transcendence-white w-5">social_leaderboard</span>
 						<h2 className="text-2xl text-transcendence-white group-hover:pt-1">tournament</h2>
