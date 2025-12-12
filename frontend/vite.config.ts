842d8941f380f5583b9876f7ebf2f13146c70643
Fred Pikkov <fpikkov@gmail.com>
2025-12-12 11:12:10 +0200

    Adjusted Vite config for a more explicit build target

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  appType: 'spa',
   plugins: [react()],
   css: { postcss: './postcss.config.js' },
+  build: {
+	outDir: 'dist',
+	sourcemap: false,
+	minify: 'esbuild'
+  },
   server: {
     host: '0.0.0.0',
     port: 8080,
