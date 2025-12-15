cd4ebf7b0de4772c6d54daefa05e754ae804e2e5
Fred Pikkov <fpikkov@gmail.com>
2025-12-15 13:44:50 +0200

    Updated docker setup for nginx. Now have to replace paths on frontend with updated backend routes.

---
+++ frontend/entrypoint.prod.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+export DOMAIN_NAME="${DOMAIN_NAME:-localhost}"
+
+# Files where environmental variables need to be substituted
+TEMPLATES= "
+	/etc/nginx/conf.d/server.conf.template;
+	/etc/nginx/snippets/security-headers.conf.template;
+"
+
+# Substitute variables within template configs
+for template in $TEMPLATES; do
+	if [ -f "$template" ]; then
+		output="${template%.template}"
+		envsubst '${DOMAIN_NAME}' < "$template" > "$output"
+	else
+		echo "Template file $template missing"
+		exit 1
+	fi
+done
+
+# Verify config
+nginx -t
+
+# Set NGINX as the foreground application
+nginx -g 'daemon off;'
