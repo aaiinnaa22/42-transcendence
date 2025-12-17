74d3d89c36347b15fabd965c5096ed40011b91f0
Aina A <aina.elona.albrecht@gmail.com>
2025-12-17 15:27:59 +0200

    custom hook for befriend/unfriend

--- frontend/src/BackendFetch.tsx
+++ frontend/src/BackendFetch.tsx
@@ -42,3 +42,51 @@ export function useBlockUser() {
 		unblockUser
 	};
 }
+
+export function useBefriendUser() {
+	async function befriendUser(toUserId: string) {
+		try {
+			const response = await fetch (`http://localhost:4241/friends/request`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				credentials: "include",
+				body: JSON.stringify({toUserId}),
+			});
+
+			const data = await response.json();
+			if (!response.ok || !data.ok)
+					throw new Error(data.error || "Failed send friend request");
+			return true;
+		}
+		catch (err: any)
+		{
+			console.log(err.message);
+		}
+	}
+
+	async function unfriendUser(friendId: string)
+	{
+		try {
+			const response = await fetch (`http://localhost:4241/friends/${friendId}`, {
+				method: "DELETE",
+				credentials: "include"
+			});
+
+			const data = await response.json();
+			if (!response.ok || !data.ok)
+					throw new Error(data.error || "Failed to remove friend");
+			return true;
+		}
+		catch (err: any)
+		{
+			console.log(err.message);
+		}
+	}
+
+	return {
+		befriendUser,
+		unfriendUser
+	};
+}
