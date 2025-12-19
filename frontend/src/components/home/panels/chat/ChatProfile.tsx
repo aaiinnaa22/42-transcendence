c0625f83a112b4bbfcb70b968fc345ed8dd2a75d
Aina A <aina.elona.albrecht@gmail.com>
2025-12-19 16:08:10 +0200

    ChatUser keeps online status between refreshUser calls

--- frontend/src/components/home/panels/chat/ChatProfile.tsx
+++ frontend/src/components/home/panels/chat/ChatProfile.tsx
@@ -16,7 +16,11 @@ export const ChatProfile = ({onExitClick, user}: ChatProfileProps) => {
 	const refreshUser = async () => {
 		const freshUser = await fetchUserFromBackend(currentUser.username);
 		if (freshUser)
-			setCurrentUser(freshUser);
+			setCurrentUser(prev => ({
+			...prev,
+			...freshUser,
+			online: prev.online,  // keep the existing online status if not included in freshUser
+		}));
 	}
 
 	useEffect(() => {
