fed4fbf788a5d7709d314a284d84e9c5cee8c412
Aina Albrecht <162609425+aaiinnaa22@users.noreply.github.com>
2025-12-19 14:45:37 +0200

    Chat profiles (#93)
    Now you can see other users profiles from the chat panel. You can block/unblock them, and befriend/unfriend them. A list of pending friend requests others has sent you, can be seen in Profile.tsx.
    
    Chat is also refactored with a new design.
    
    Everything should be responsive.

--- frontend/src/components/home/panels/chat/ChatProfilePic.tsx
+++ frontend/src/components/home/panels/chat/ChatProfilePic.tsx
@@ -1,23 +1,20 @@
-type Friend = {
-	username: string;
-	profile: string;
-	online?: boolean;
-	lastMessage?: string;
-}
+import type { ChatUser } from "./ChatContainer";
 
 type ChatProfileProps = {
-	friend: Friend;
+	friend: ChatUser;
+	onProfileClick: (user: ChatUser) => void;
 }
 
-export const ChatProfilePic = ({friend}: ChatProfileProps) =>
+export const ChatProfilePic = ({friend, onProfileClick}: ChatProfileProps) =>
 {
 	return (
-		<div className="relative inline-block h-9 w-9 lg:h-12 lg:w-12">
-			{friend.profile !== ""
-			? <img className="ml-1 mt-1 w-7 h-7 lg:w-10 lg:h-10 rounded-full object-cover" src="/testimage.png"></img>
-			: <span className="!text-4xl lg:!text-5xl material-symbols-outlined">account_circle</span>
+		<div className="relative inline-block h-14 w-14 cursor-pointer"
+			onClick={() => onProfileClick(friend)}>
+			{friend.profile != ""
+			? <img className="ml-1 mt-1 w-12 h-12 rounded-full object-cover" src={friend.profile}></img>
+			: <span className="!text-6xl material-symbols-outlined">account_circle</span>
 			}
-			<div className={"absolute top-1 left-1 border-[1.5px] rounded-full w-2 h-2 lg:w-3 lg:h-3 " + (friend.online ? "bg-purple-500" : "bg-white")}></div>
+			<div className={"absolute top-1 left-1 border-[1.5px] rounded-full w-3 h-3 " + (friend.online ? "bg-transcendence-green" : "bg-transcendence-red")}></div>
 		</div>
 	);
 }
