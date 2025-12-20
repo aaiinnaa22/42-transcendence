6d0eaf4f77a48ead8ce29aba6f78d425ed5d8ecc
LEE CHI <applerich0306@gmail.com>
2025-12-20 23:57:50 +0200

    [feature] Delete (#98)
    * [feature] Delete
    
    - user can delete themslef -> navigate to welcome page with cookies cleared
    
    note: I did not write clear-cookies() in delete("/users/me") because when frontend calls force-logout() it has that clear() call already. However, this can be discussed
    
    * clean up
    
    ---------
    
    Co-authored-by: LEE CHI <chi@rnd.works>

--- frontend/src/components/home/settings/Settings.tsx
+++ frontend/src/components/home/settings/Settings.tsx
@@ -3,12 +3,14 @@ import { useNavigate } from "react-router-dom";
 import {LanguageSelector} from "./LanguageSelector"
 import { TwoFAModal } from "./TwoFAModal";
 import { fetchWithAuth } from "../../../api/fetchWithAuth";
+import { DeleteAccountModal } from "./DeleteAccountModal";
 
 export const Settings = () =>
 {
 	const [error, setError] = useState<string | null>(null);
 	const [isTwoFAModalOpen, setIsTwoFAModalOpen] = useState(false);
 	const [isTwoFAEnabled, setIsTwoFAEnabled] = useState<boolean | null>(null);
+	const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 	const navigate = useNavigate();
 
 	useEffect(() => {
@@ -84,7 +86,11 @@ export const Settings = () =>
 						onClick={handleLogOut}>
 						Log out
 					</button>
-					<button className="landscape:text-left lg:landscape:text-center text-transcendence-red font-transcendence-two text-sm landscape:text-xs lg:landscape:text-sm font-semibold cursor-pointer hover:font-bold w-full">Delete account</button>
+					<button
+						className="landscape:text-left lg:landscape:text-center text-transcendence-red font-transcendence-two text-sm landscape:text-xs lg:landscape:text-sm font-semibold cursor-pointer hover:font-bold w-full"
+						onClick={() => setIsDeleteModalOpen(true)}>
+							Delete account
+					</button>
 				</div>
 			</div>
             <TwoFAModal
@@ -93,6 +99,10 @@ export const Settings = () =>
                 onClose={() => setIsTwoFAModalOpen(false)}
                 onStatusChange={handleTwoFAStatusChange}
             />
+				<DeleteAccountModal
+					isOpen={isDeleteModalOpen}
+					onClose={() => setIsDeleteModalOpen(false)}
+				/>
 		</>
 	);
 }
