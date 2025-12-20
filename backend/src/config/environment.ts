556a581738db31dd7cf25d92dcedc6256862db1c
LEE CHI <chi@rnd.works>
2025-12-20 14:04:26 +0200

    works without adding more nginx route

--- backend/src/config/environment.ts
+++ backend/src/config/environment.ts
@@ -75,7 +75,7 @@ const PUBLIC_URL = isProduction
 	: `${HTTP_PROTO}${_env.PUBLIC_DOMAIN}:${_env.PORT}`;
 
 // Public callback path served by Nginx and proxied to backend /api/auth
-const GOOGLE_CALLBACK_URL = `${PUBLIC_URL}/auth/google/callback`;
+const GOOGLE_CALLBACK_URL = `${PUBLIC_URL}/api/auth/google/callback`;
 
 // Export the validated environment
 export const env = {
