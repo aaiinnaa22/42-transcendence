a966a9363ba28869bda7a16c1baba2de6ce6ca4c
Fred Pikkov <fpikkov@gmail.com>
2025-12-24 15:37:22 +0200

    Created translations, will apply them on a separate branch due to larger refactor

--- frontend/src/components/home/game/Waiting.tsx
+++ frontend/src/components/home/game/Waiting.tsx
@@ -1,12 +1,18 @@
+import { useTranslation } from "react-i18next";
+
 interface WaitingProps {
 	opponent: string;
 }
 
 export const Waiting = ({ opponent }: WaitingProps) =>
 {
+	const {t} = useTranslation();
+
 	return (
 		<div className="relative w-full h-[calc(100svh-4.5rem)] lg:h-[calc(100svh-8rem)] flex justify-center items-center">
-			<h2 className="text-transcendence-white font-transcendence-three text-3xl tracking-[0.12em] text-center">Waiting for {opponent}...</h2>
+			<h2 className="text-transcendence-white font-transcendence-three text-3xl tracking-[0.12em] text-center">
+				{t("game.condition.waiting", { player: opponent })}
+			</h2>
 		</div>
 	);
 };
