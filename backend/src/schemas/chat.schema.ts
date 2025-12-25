e32185efc078e2ab5ac91d150887757f041f7813
Fred Pikkov <fpikkov@gmail.com>
2025-12-25 15:47:36 +0200

    Applied schema validation to chatComponent (#108)

--- backend/src/schemas/chat.schema.ts
+++ backend/src/schemas/chat.schema.ts
@@ -1,9 +1,28 @@
 import { z } from "zod";
-import { usernameField } from "../shared/utility/validation.utility.js";
+import { idField, usernameField } from "../shared/utility/validation.utility.js";
 
 
 export const ChatUsersUsernameSchema = z.object( {
 	username: usernameField
 } );
 
+export const ChatDirectMessageSchema = z.object( {
+	type: z.literal( "dm" ),
+	to: idField,
+	message: z.string.min( 1 ).max( 600 )
+} );
+
+export const ChatInviteMessageSchema = z.object( {
+	type: z.literal( "invite" ),
+	to: idField
+} );
+
+export const ChatClientMessageSchema = z.discriminatedUnion( "type", [
+	ChatDirectMessageSchema,
+	ChatInviteMessageSchema
+] );
+
 export type ChatUsersUsernameQuery = z.infer<typeof ChatUsersUsernameSchema>;
+export type ChatDirectMessageInput = z.infer<typeof ChatDirectMessageSchema>;
+export type ChatInviteMessageInput = z.infer<typeof ChatInviteMessageSchema>;
+export type ChatClientessageInput = z.infer<typeof ChatClientMessageSchema>;
