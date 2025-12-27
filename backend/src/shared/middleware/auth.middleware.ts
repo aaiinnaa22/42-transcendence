1cf3bbbda48ba8f2d69f26883e09eee314e9d2a2
Fred Pikkov <fpikkov@gmail.com>
2025-12-27 14:28:46 +0200

    Added missing port number in NGINX CSP. Removed excess protocol from the public URL in CORS. (#113)
    Included verbose logging by auth middleware.
    
    Overhauled NGINX rate limiting.

--- backend/src/shared/middleware/auth.middleware.ts
+++ backend/src/shared/middleware/auth.middleware.ts
@@ -3,28 +3,28 @@ import type { FastifyRequest, FastifyReply } from "fastify";
 export const authenticate = async ( request: FastifyRequest, reply: FastifyReply ) =>
 {
 	const signed = ( request.cookies as any )?.accessToken;
-	if ( signed )
+	if ( !signed )
 	{
-		const unsign = request.unsignCookie( signed );
-		if ( unsign.valid )
-		{
-			try
-			{
-				const payload = request.server.jwt.verify( unsign.value );
-				( request as any ).user = payload;
-				return;
-			}
-			catch
-			{
-				request.log.debug( "cookie-contained-jwt-verify-failed" );
-			}
-		}
-		else
-		{
-			request.log.debug( "accessToken cookie signature invalid" );
-		}
+		request.log.info( "accessToken missing" );
+		return reply.code( 401 ).send( { error: "Unauthorized" } );
 	}
 
-	request.log.error( "JWT verify failed" );
-	reply.code( 401 ).send( { error: "Unauthorized" } );
+	const unsign = request.unsignCookie( signed );
+	if ( !unsign.valid )
+	{
+		request.log.warn( "accessToken cookie signature invalid" );
+		return reply.code( 401 ).send( { error: "Unauthorized" } );
+	}
+
+	try
+	{
+		const payload = request.server.jwt.verify( unsign.value );
+		( request as any ).user = payload;
+		return;
+	}
+	catch
+	{
+		request.log.warn( "cookie-contained-jwt-verify-failed" );
+		return reply.code( 401 ).send( { error: "Unauthorized" } );
+	}
 };
